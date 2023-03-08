c0f851f33408c59cbc0cd2d3491a373f48237185
Antonio Moore <P2658866@my365.dmu.ac.uk>
2023-03-08 10:57:38 +0000

    Added more to the presentation layer

--- AdminSystem/CustomerViewer.aspx.cs
+++ AdminSystem/CustomerViewer.aspx.cs
@@ -4,11 +4,20 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1Viewer : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
-
+        clsCustomer ACustomer = new clsCustomer();
+        ACustomer = (clsCustomer)Session["ACustomer"];
+        string NewLine = ("\r\n");
+        Response.Write(ACustomer.CustomerName + NewLine);
+        Response.Write(ACustomer.AddressLine1 + NewLine);
+        Response.Write(ACustomer.PostCode + NewLine);
+        Response.Write(ACustomer.EmailAddress + NewLine);
+        Response.Write(ACustomer.EmailVerification + NewLine);
+        Response.Write(ACustomer.AccountCreationDate + NewLine);
     }
 }
