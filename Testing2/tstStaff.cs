264b34c03a2f8d437521c8632475cd2985711422
Alfie Nickson <P2661399@my365.dmu.ac.uk>
2023-03-08 10:36:00 +0000

    clsStaff

---
+++ Testing2/tstStaff.cs
@@ -0,0 +1,19 @@
+﻿using ClassLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace Testing2
+{
+    [TestClass]
+    public class tstStaff
+    {
+        [TestMethod]
+        public void InstanceOK()
+        {
+            clsStaff staff = new clsStaff();
+
+            Assert.IsNotNull(staff);
+        }
+        }
+    }
+
