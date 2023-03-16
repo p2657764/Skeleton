9d4abbbaa84e556850cf3e10835696f71d5feba1
p2661017 <p2661017@my365.dmu.ac.uk>
2023-03-16 09:52:00 +0000

    Added presentation layer

--- AdminSystem/StockDataEntry.aspx.cs
+++ AdminSystem/StockDataEntry.aspx.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using ClassLibrary;
 
 public partial class _1_DataEntry : System.Web.UI.Page
 {
@@ -11,4 +13,24 @@ public partial class _1_DataEntry : System.Web.UI.Page
     {
 
     }
+
+    
+
+    protected void btnOK_Click(object sender, EventArgs e)
+    {
+        //create a new instance of clsAddress
+        clsStock Stock = new clsStock();
+        //capture the product name
+        Stock.StockName = txtProductName.Text;
+        Stock.StockColour = txtColour.Text;
+        Stock.UnitPrice = Convert.ToInt32(txtUnitPrice.Text); 
+        Stock.StockNo = Convert.ToInt32(txtStockQuantity.Text);
+        Stock.DateAdded = Convert.ToDateTime(txtDateGenerated.Text);
+        Stock.Active = chkInStock.Checked;
+        //store the stock in the session object
+        Session["Stock"] = Stock;
+        //navigate to the viewer page
+        Response.Redirect("StockViewer.aspx");
+    }
+
 }
