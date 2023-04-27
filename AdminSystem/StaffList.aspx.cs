bf0126aafa91b8a4b69bde66370a11e1ec3a9643
Alfie Nickson <P2661399@my365.dmu.ac.uk>
2023-04-27 21:34:50 +0100

    Collection

--- AdminSystem/StaffList.aspx.cs
+++ AdminSystem/StaffList.aspx.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using ClassLibrary;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -9,6 +10,20 @@ public partial class _1_List : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
     {
+        //if this is the first time the page is displayed
+        if (IsPostBack == false)
+        {
+            //update the list box
+            DisplayStaff();
+        }
 
+    }
+    void DisplayStaff()
+    {
+        clsStaffCollection staff = new clsStaffCollection();
+        lstStaffList.DataSource = staff.StaffList;
+        lstStaffList.DataValueField = "StaffNo";
+        lstStaffList.DataTextField = "Name";
+        lstStaffList.DataBind();
     }
 }
